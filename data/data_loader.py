e0731feb044d78727134e370cea4ca026fc8667a
kamiertop <kamiertop@gmail.com>
2026-03-23 16:55:54 +0800

    :sparkles: 完成Dataset

--- data/data_loader.py
+++ data/data_loader.py
@@ -1,8 +1,11 @@
 from pathlib import Path
+from typing import Callable
 
 import polars as pl
+import torch.cuda
+from PIL import Image
 from pydantic import BaseModel
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, DataLoader
 
 from utils import Config
 
@@ -26,29 +29,40 @@ class InsectDataset(Dataset):
         class_num: 有多少类别，支持通过order_name指定‘目’
     """
 
-    def __init__(self, config: Config, order_name: str = ""):
+    def __init__(self, config: Config, split: str = 'train', transform: Callable = None):
+
+        assert split in ['train', 'val', 'test']
+        self.transform = transform
         self.dataset_path: str = config.datasets_path
-        self.df: pl.DataFrame = pl.read_csv(Path(config.data_root) / "label.csv")
+        self.df: pl.DataFrame = pl.read_csv(Path(config.data_root) / f"{split}.csv")
         self.data: list = []
-        self.class_num: int = self.class_num_by_order(order_name=order_name)
 
-    def class_num_by_order(self, order_name: str) -> int:
-        """
-        获取一个‘目’下面有多少种 (即 label 的唯一值数量)
-        """
+        if self.df.height == 0:
+            raise ValueError(f"{split} has no rows")
 
-        return (
-            self
-            .df
-            .select(["order", "label"])
-            .filter(pl.col("order") == order_name)
-            .select("label")
-            .n_unique()
-        )
+        classes: list[str] = self.df.select("label").drop_nulls().unique().to_series().to_list()
+        self.class_to_idx: dict[str, int] = {}
+        self.idx_to_class: dict[int, str] = {}
+        for i, name in enumerate(classes):
+            self.class_to_idx[name] = i
+            self.idx_to_class[i] = name
+
+        self.samples: list[dict] = self.df.select(["path", "label", "group_id"]).to_dicts()
 
     def __len__(self) -> int:
-        return len(self.data)
+        return len(self.samples)
 
-    # TODO
     def __getitem__(self, index: int):
-        pass
+        row: dict = self.samples[index]
+        img = Image.open(row["path"]).convert("RGB")
+        if self.transform is not None:
+            img = self.transform(img)
+        class_idx: int = self.class_to_idx[row["label"]]
+
+        return img, class_idx, {"path": row["path"], "group_id": row["group_id"]}
+
+
+def build_dataloader(dataset: InsectDataset, shuffle=False, batch_size=32, num_workers=4) -> DataLoader:
+    return DataLoader(
+        dataset, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers, pin_memory=torch.cuda.is_initialized()
+    )
